e1afd760ad513a674a1ee8839b49945dc622cf5d
Weuller <weullermarcos@hotmail.com>
2024-08-26 13:26:38 -0300

    Criando componente de filtro

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { View, TouchableOpacityProps } from 'react-native';
-
-import { Container, Icon } from './styles';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Container, Icon, ButtonIconTypeStyleProps } from './styles';
 
 type Props = TouchableOpacityProps & {
-
+  icon: keyof typeof MaterialIcons.glyphMap; //criando um tipo correspondente a bibliteca de imagens
+  type?: ButtonIconTypeStyleProps; //opcional
 };
 
-export function ButtonIcon({}: Props) {
+export function ButtonIcon({icon, type = 'PRIMARY', ...rest}: Props) {
   return (
-    <Container>
-        <Icon name='home' type='SECONDARY' />
+    <Container {...rest}>
+        <Icon 
+          name={icon} 
+          type={type} 
+        />
     </Container>
   );
 }
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Container, Form } from './styles';
 
-import { Container } from './styles';
 import { Header } from '@components/Header';
 import { HighLight } from '@components/HighLight';
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
 
 export function Players() {
   return (
@@ -16,7 +17,12 @@ export function Players() {
             subtitle='Adicione a galera e separe os times'
         />
 
-        <ButtonIcon />
+        <Form>
+          <Input placeholder='Nome da pessoa' autoCorrect={false}/>
+          <ButtonIcon icon='add' type='PRIMARY'/>
+        </Form>
+
+        <Filter title='Time A' isActive={true}/>
 
     </Container>
   );

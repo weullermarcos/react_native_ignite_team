ed43f6c5d48ec8135c1f053ac1060424a81698ab
Weuller <weullermarcos@hotmail.com>
2024-10-17 12:33:07 -0300

    finalizando componente de PlayerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -8,11 +8,13 @@ import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
 import { PlayerCard } from '@components/PlayerCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Players() {
 
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Adamastor', 'Adelmira']);
+  const [players, setPlayers] = useState(['Adamastor', 'Adelmira', 'Abadia', 'Adalberto', 'Amanda', 'Albertão', 'Helismar']);
 
   return (
     <Container>
@@ -54,7 +56,12 @@ export function Players() {
           renderItem={({ item }) => (
             <PlayerCard name={item} onRemove={() => {}}/>
           )}
+          ListEmptyComponent={() => <ListEmpty message='Não há pessoas nesse time!'/>}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 } ]}
         />
+
+        <Button title='Remover turma' type='SECONDARY' />
 
     </Container>
   );

49393277a7eabe387c9cb630aabd576d326491d1
Weuller <weullermarcos@hotmail.com>
2025-01-07 11:21:21 -0300

    Usando SafeAreaView da react-native-safe-area-context

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,7 +1,9 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 //Importando styled components
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;

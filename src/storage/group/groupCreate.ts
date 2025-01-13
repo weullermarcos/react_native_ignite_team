1bbc214fa3f1bf5e63518c65f9fefe3fc6baed67
Weuller <weullermarcos@hotmail.com>
2025-01-13 17:39:59 -0300

    Criando arquivo de exceções

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,12 +1,19 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "@storage/storageConfig";
 import { groupsGetAll } from "./groupsGetAll";
+import { AppError } from "@utils/AppError";
 
 export async function groupCrate(newGroup: string){
 
     try{
 
         const storedGroups = await groupsGetAll(); //Recupera os grupos armazenados
+
+        const groupAlreadyExists = storedGroups.includes(newGroup); //Verifica se o novo grupo já existe nos grupos armazenados
+
+        if(groupAlreadyExists){
+            throw new AppError('Já existe um grupo cadastrado com esse nome!'); //Lança uma exceção caso o grupo já exista
+        }
 
         const storage = JSON.stringify([...storedGroups, newGroup]); //Concatena os grupos existentes com o novo grupo
 
